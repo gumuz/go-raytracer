d688d25a2a5cf6240dae59c024b214e85aab2b3a
Guyon Moree <guyon.moree@gmail.com>
2019-12-12 15:14:54 +0100

    render a ball n 3D!

--- cmd/ball/ball.go
+++ cmd/ball/ball.go
@@ -13,8 +13,10 @@ func main() {
 	half := wallSize / 2
 
 	canvas := rt.NewCanvas(canvasPixels, canvasPixels)
-	white := &rt.Color{1, 1, 1}
 	sphere := rt.NewSphere()
+	sphere.Material.Color = &rt.Color{1, 0.7, 1}
+	light := rt.NewPointLight(rt.NewPoint(-10, 10, -10), &rt.Color{1, 1, 1})
+
 	// sphere.SetTransform(rt.Scaling(0.5, 1, 1).Mul(rt.RotationZ(math.Pi / 4)).Mul(rt.Shearing(1, 0, 0, 0, 0, 0)))
 
 	// worldX, worldY := 0.0, 0.0
@@ -25,9 +27,16 @@ func main() {
 			position := rt.NewPoint(worldX, worldY, wallZ)
 			ray := rt.NewRay(rayOrigin, position.Sub(rayOrigin).Norm())
 			xs := sphere.Intersect(ray)
+			hit := xs.Hit()
 
-			if xs.Hit() != nil {
-				canvas.SetAt(x, y, white)
+			if hit != nil {
+				point := ray.Pos(hit.T)
+				normal := hit.Object.NormalAt(point)
+				eye := ray.Direction.Neg()
+
+				color := hit.Object.GetMaterial().Lighting(light, point, eye, normal)
+
+				canvas.SetAt(x, y, color)
 			}
 		}
 	}
